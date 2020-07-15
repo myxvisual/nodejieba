7e91acb1aad7eb2de02621624b0bf423a9164ad1
myxvisual <myxvisual@live.com>
2020-07-15 14:19:44 +0800

    feat: Add memeory

--- wasm/test.js
+++ wasm/test.js
@@ -25,10 +25,11 @@ function mapToObject(map) {
 
     return obj
 }
-
-emModule.onRuntimeInitialized = function() {
-    // emModule['FS_createDataFile']("/tmp", "test.txt", "test-content", true, true)
-    // emModule.testReadFile();
+async function test() {
+    const Module = await emModule();
+    
+    // Module['FS_createDataFile']("/tmp", "test.txt", "test-content", true, true)
+    // Module.testReadFile();
     const DEFAULT_DICT = fs.readFileSync(path.join(__dirname, "../dict/jieba.dict.utf8"), "utf-8")
     const DEFAULT_HMM_DICT = fs.readFileSync(path.join(__dirname, "../dict/hmm_model.utf8"), "utf-8")
     const DEFAULT_USER_DICT = fs.readFileSync(path.join(__dirname, "../dict/user.dict.utf8"), "utf-8")
@@ -36,15 +37,16 @@ emModule.onRuntimeInitialized = function() {
     const DEFAULT_STOP_WORD_DICT = fs.readFileSync(path.join(__dirname, "../dict/stop_words.utf8"), "utf-8")
 
     const now = Date.now()
-    const loadRes = emModule.load(DEFAULT_DICT, DEFAULT_HMM_DICT, DEFAULT_USER_DICT, DEFAULT_IDF_DICT, DEFAULT_STOP_WORD_DICT)
+    const loadRes = Module.load(DEFAULT_DICT, DEFAULT_HMM_DICT, DEFAULT_USER_DICT, DEFAULT_IDF_DICT, DEFAULT_STOP_WORD_DICT)
     console.log(loadRes)
     console.log(`${Date.now() - now}ms`)
     // ~3616 ms --> ~2789ms
 
-    const cutVec = emModule.cut("南京市长江大桥", false);
+    const cutVec = Module.cut("南京市长江大桥", false);
     const cutArr = vectorToArray(cutVec)
     console.log(cutArr)
-    const tagMap = emModule.tag("红掌拨清波")
+    const tagMap = Module.tag("红掌拨清波")
     const tagObj = mapToObject(tagMap)
     console.log(tagObj)
 }
+test();
